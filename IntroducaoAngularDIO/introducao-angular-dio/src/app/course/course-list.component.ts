b9650137e2a0833ed29b6bcf711d69199c1f50aa
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-02-19 19:23:53 -0300

    listagem de cursos

--- IntroducaoAngularDIO/introducao-angular-dio/src/app/course/course-list.component.ts
+++ IntroducaoAngularDIO/introducao-angular-dio/src/app/course/course-list.component.ts
@@ -1,9 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
 
 @Component({
     selector: 'app-course-list', 
-    templateUrl: './course-list.component.html'
+    templateUrl: './course-list.component.html',
+    styleUrls: ['./course-list.component.css']
 })
-export class CourseListComponent{
+export class CourseListComponent implements OnInit{
+   
+    courses: Course[] = [];
 
+    ngOnInit(): void {
+        this.courses= [
+            {
+                id: 1,
+                name: "Introdução ao Angular",
+                imageUrl: "url Omage",
+                price: 24.99,
+                code: "code introdução ao angular",
+                duration: 5,
+                rating: 4.5,
+                releaseData: "Janeiro/2020"
+            },
+
+            {
+                id: 2,
+                name: "Introdução ao Java",
+                imageUrl: "url Image",
+                price: 14.99,
+                code: "code introdução ao java",
+                duration: 12,
+                rating: 4.5,
+                releaseData: "fevereiro/2021"
+            },
+
+            {
+                id: 3,
+                name: "Introdução ao Cobol",
+                imageUrl: "url Image",
+                price: 444.99,
+                code: "code introdução ao Cobol",
+                duration: 50,
+                rating: 5,
+                releaseData: "junho/2014"
+            }
+        ]
+    }
 }
