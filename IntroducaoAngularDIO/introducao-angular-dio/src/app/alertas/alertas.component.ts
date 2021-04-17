ca1b819e4fab08574086368e1946950ff9db4aa6
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-04-17 18:12:30 -0300

    alertar componente

---
+++ IntroducaoAngularDIO/introducao-angular-dio/src/app/alertas/alertas.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+
+@Component({
+  selector: 'app-alertas',
+  templateUrl: './alertas.component.html',
+  styleUrls: ['./alertas.component.css']
+})
+export class AlertasComponent implements OnInit {
+
+  constructor(
+    public modal: BsModalRef
+  ) { }
+
+  @Input() message: string;
+  @Input() type: string;
+
+  ngOnInit(): void {
+  }
+  
+  onClose():void{
+    this.modal.hide();
+  }
+
+}
