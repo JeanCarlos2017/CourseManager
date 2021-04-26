9052c933eac9752178ebd5cea0e062c2cf13616a
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-04-26 15:41:15 -0300

    Update course-list.component.ts
    busca e listagem de cursos

--- IntroducaoAngularDIO/introducao-angular-dio/src/app/course/course-list.component.ts
+++ IntroducaoAngularDIO/introducao-angular-dio/src/app/course/course-list.component.ts
@@ -4,44 +4,54 @@ import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 import { Course } from '../model/course';
 import { AlertasService } from '../service/alertas.service';
-import { CourseService } from './course.service';
+import { CourseService } from '../service/course.service';
+
 //imports para colocar o R$
 
 @Component({
     templateUrl: './course-list.component.html',
     styleUrls: ['./course-list.component.css']
 })
-export class CourseListComponent implements OnInit{
-    filteredCourses: Course[]= [];
+export class CourseListComponent implements OnInit {
+    filteredCourses: Course[] = [];
 
-    _courses: Course[] = [];
-    constructor(private courseService: CourseService, private alertService: AlertasService, 
-        private title: Title, private router: Router){}
+    _courses: Course[];
+    filter: string;
+    constructor(private courseService: CourseService, private alertService: AlertasService,
+        private title: Title, private router: Router) { }
 
-    _filterBy: string;
+
+
     ngOnInit(): void {
-       window.scroll(0,0);
-       this.title.setTitle("Todos os cursos da plataforma Course Manager");
-       this.isLogado();       
-       this._courses= this.courseService.retrieveAll();
-       this.filteredCourses= this._courses;
-      
+        window.scroll(0, 0);
+        this.title.setTitle("Todos os cursos da plataforma Course Manager");
+        this.isLogado();
+        this.findAllCourse();
+
     }
 
-    set filter(value: string){
-        this._filterBy= value;
-        this.filteredCourses=  this._courses.filter( (course: Course) => 
-                course.name.toLowerCase().indexOf(this._filterBy.toLowerCase()) > -1);
-    }
-
-    get filter(){
-        return this._filterBy;
-    }
-
-    isLogado(){
-        if(environment.token === ''){
+    isLogado() {
+        if (environment.token === '') {
             this.alertService.showAlert("Sua seção foi encerrada, faça o login novamente", "info")
             this.router.navigate(['/entrar']);
+        }
+    }
+
+    findAllCourse() {
+        this.courseService.findAllCourses().subscribe((resp: Course[]) => {
+            this._courses = resp;
+            this.filteredCourses = this._courses;
+        })
+    }
+
+    findByNome() {
+        if (this.filter === '') {
+            this.filteredCourses = this._courses;
+        }
+        else {
+            this.courseService.filterByNome(this.filter).subscribe((resp: Course[]) => {
+                this.filteredCourses = resp;
+            })
         }
     }
 }
