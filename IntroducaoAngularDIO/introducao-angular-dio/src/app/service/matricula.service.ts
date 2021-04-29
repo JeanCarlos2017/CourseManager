783f5192cb35ba44f3229b2987af25bda2eb4b54
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-04-28 22:35:54 -0300

    serviço de listar e add matricula

---
+++ IntroducaoAngularDIO/introducao-angular-dio/src/app/service/matricula.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Course } from '../model/course';
+import { Matricula } from '../model/Matricula';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MatriculaService {
+  constructor(private http: HttpClient) { }
+
+  idUser: number= environment.id;
+
+  token= {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  matricularAluno(idCourse: number): Observable<Matricula>{
+    return this.http.post<Matricula>(`http://localhost:8080/usuario/${this.idUser}/matricular/curso/${idCourse}`, this.token)
+  }
+
+  listaMatricula(): Observable<Matricula[]>{
+    return this.http.get<Matricula[]>(`http://localhost:8080/usuario/${this.idUser}/matricular/listar`, this.token)
+  }
+}
