e4ba351cf6ea726673cf47abd75d520b7cc88ad8
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-04-28 23:01:18 -0300

    opção de finalizar curso

--- IntroducaoAngularDIO/introducao-angular-dio/src/app/course-opened/course-opened.component.ts
+++ IntroducaoAngularDIO/introducao-angular-dio/src/app/course-opened/course-opened.component.ts
@@ -20,53 +20,55 @@ export class CourseOpenedComponent implements OnInit {
   _courses: Course[];
   filter: string;
   constructor(private courseService: CourseService, private alertService: AlertasService,
-      private title: Title, private router: Router, private matriculaService: MatriculaService) { }
+    private title: Title, private router: Router, private matriculaService: MatriculaService) { }
 
 
 
   ngOnInit(): void {
-      window.scroll(0, 0);
-      this.title.setTitle("Todos os cursos da plataforma Course Manager");
-      this.isLogado();
-      this.getCourseOpenedUser();
+    window.scroll(0, 0);
+    this.title.setTitle("Todos os cursos da plataforma Course Manager");
+    this.isLogado();
+    this.getCourseOpenedUser();
   }
 
   isLogado() {
-      if (environment.token === '') {
-          this.alertService.showAlert("Sua seção foi encerrada, faça o login novamente", "info")
-          this.router.navigate(['/entrar']);
-      }
+    if (environment.token === '') {
+      this.alertService.showAlert("Sua seção foi encerrada, faça o login novamente", "info")
+      this.router.navigate(['/entrar']);
+    }
   }
 
   getCourseOpenedUser() {
-      this.courseService.getCourseOpenedUser().subscribe((resp: Course[]) => {
-          this._courses = resp;
-          this.filteredCourses = this._courses;
-      })
+    this.courseService.getCourseOpenedUser().subscribe((resp: Course[]) => {
+      this._courses = resp;
+      this.filteredCourses = this._courses;
+    })
   }
 
   findByNome() {
-      if (this.filter === '') {
-          this.filteredCourses = this._courses;
-      }
-      else {
-          this.courseService.filterByNome(this.filter).subscribe((resp: Course[]) => {
-              this.filteredCourses = resp;
-          })
-      }
+    if (this.filter === '') {
+      this.filteredCourses = this._courses;
+    }
+    else {
+      this.courseService.filterByNome(this.filter).subscribe((resp: Course[]) => {
+        this.filteredCourses = resp;
+      })
+    }
   }
 
-matricular(idCourse: number){
-      this.matriculaService.matricularAluno(idCourse).subscribe( (resp: Matricula)=>{
-          console.log(resp)
-      }, erro =>{
-          if(erro.status === 400){
-            erro.error.campos.forEach( (campo)=>{
-              this.alertService.showAlert('mensagem: '+campo.mensagem, 'danger')
-            })
-            
-          }
-        });
+  finalizar(idCourse: number) {
+    this.matriculaService.finalizarCurso(idCourse).subscribe( () => {
+      this.alertService.showAlert("Curso finalizado com sucesso", "success")
+      this.getCourseOpenedUser();
+      
+    }, erro => {
+      if (erro.status === 400) {
+        erro.error.campos.forEach((campo) => {
+          this.alertService.showAlert('mensagem: ' + campo.mensagem, 'danger')
+        })
+
+      }
+    });
   }
 
 }
