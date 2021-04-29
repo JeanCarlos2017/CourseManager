6dec28fdb241f0d3fcc818bbb1f85808b45be049
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-04-29 10:40:02 -0300

    Update course.service.ts
    add baseUrl para ajudar no deploy

--- IntroducaoAngularDIO/introducao-angular-dio/src/app/service/course.service.ts
+++ IntroducaoAngularDIO/introducao-angular-dio/src/app/service/course.service.ts
@@ -13,35 +13,37 @@ export class CourseService {
 
   idUser: number= environment.id;
 
+  baseUrl: string= 'http://localhost:8080/usuario/';
+
   token= {
     headers: new HttpHeaders().set('Authorization', environment.token)
   }
 
   findAllCourses(): Observable<Course[]>{
-    return this.http.get<Course[]>(`http://localhost:8080/usuario/${this.idUser}/cursos`, this.token)
+    return this.http.get<Course[]>(this.baseUrl.concat(`${this.idUser}/cursos`), this.token)
   }
 
   filterByNome(nome: string): Observable<Course[]>{
-    return this.http.get<Course[]>(`http://localhost:8080/usuario/${this.idUser}/cursos/pesquisa-curso-por/${nome}`, this.token)
+    return this.http.get<Course[]>(this.baseUrl.concat(`${this.idUser}/cursos/pesquisa-curso-por/${nome}`), this.token)
   }
 
   saveCourse(course: Course): Observable<Course>{
-    return this.http.post<Course>(`http://localhost:8080/usuario/${this.idUser}/cursos/cadastrar`, course, this.token)
+    return this.http.post<Course>(this.baseUrl.concat(`${this.idUser}/cursos/cadastrar`), course, this.token)
   }
 
   findCourseById(courseId: number): Observable<Course>{
-    return this.http.get<Course>(`http://localhost:8080/usuario/${this.idUser}/cursos/buscaPorId/${courseId}`, this.token)
+    return this.http.get<Course>(this.baseUrl.concat(`${this.idUser}/cursos/buscaPorId/${courseId}`), this.token)
   }
 
   putCourse(course: Course): Observable<Course>{
-    return this.http.put<Course>(`http://localhost:8080/usuario/${this.idUser}/cursos/alterar`, course, this.token)
+    return this.http.put<Course>(this.baseUrl.concat(`${this.idUser}/cursos/alterar`), course, this.token)
   }
   
   getCourseOpenedUser():Observable<Course[]>{
-    return this.http.get<Course[]>(`http://localhost:8080/usuario/${this.idUser}/cursos/pendente`, this.token)
+    return this.http.get<Course[]>(this.baseUrl.concat(`${this.idUser}/cursos/pendente`), this.token)
   }
 
   getCourseClosedUser():Observable<Course[]>{
-    return this.http.get<Course[]>(`http://localhost:8080/usuario/${this.idUser}/cursos/finalizados`, this.token)
+    return this.http.get<Course[]>(this.baseUrl.concat(`${this.idUser}/cursos/finalizados`), this.token)
   }
 }
